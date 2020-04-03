f07e3339b91248378698f11ea8c8f6ce950c967a
yasiru98 <yasiru043@gmail.com>
2020-04-03 03:10:13 -0400

    domomaker B bug fixing

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,3 +1,6 @@
+const models = require('../models');
+const Domo = models.Domo;
+
 const makerPage = (req,res) =>{
     Domo.DomoModel.findByOwner(req.session.account._id, (err,docs)=>{
         if(err){
@@ -9,8 +12,7 @@ const makerPage = (req,res) =>{
     });
 
 };
-const models = require('../models');
-const Domo = models.Domo;
+
 
 const makeDomo = (req, res) => {
     if(!req.body.name || !req.body.age){
